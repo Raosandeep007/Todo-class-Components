371eb4f0e22dfd782905112adbb77fd850c008f1
Sandeep Yadav <yadavsandeep775@gmail.com>
2022-03-25 20:59:49 +0530

    final

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -22,22 +22,27 @@ export class Todo extends React.Component {
   // on mounting I want to diaplay all the data
   handleAdd() {
     var { query } = this.state;
-    if (Object.keys(query).length > 0) {
-      if (!query.time) {
-        alert("🚫set a date and time for completion");
+    if (Object.keys(query).length > 0 && query.task !== undefined) {
+      if (query.date == undefined) {
+        alert("🚫set a date for completion");
+      } else if (query.time == undefined) {
+        alert("🚫set a time for completion");
       } else {
         const payload = {
           title: query,
           status: false,
         };
-        axios
-          .post("https://shadow-glittery-bosworth.glitch.me/todos", payload)
-          .then((res) => {
-            this.handleGetTodos();
-          });
+        console.log('"query.time":', query.time);
+        console.log('"query.date":', query.date);
+        console.log("payload:", payload);
+        // axios
+        //   .post("https://shadow-glittery-bosworth.glitch.me/todos", payload)
+        //   .then((res) => {
+        //     this.handleGetTodos();
+        //   });
       }
     } else {
-      alert("Nothing to add");
+      alert("🚫Nothing to add");
     }
   }
 
@@ -109,7 +114,8 @@ export class Todo extends React.Component {
   render() {
     const { todo, query } = this.state;
     return (
-      <div>
+      <div id="main">
+        
         <div id="addtododiv">
           <h1 id="heading">ADD YOUR TODO</h1>
           <div id="adddiv">
@@ -130,14 +136,24 @@ export class Todo extends React.Component {
               required
               placeholder="Add Something"
             />
-            <input
-              id="inputtodo"
-              type="datetime-local"
-              name="time"
-              onChange={(e) => {
-                this.handleChange(e);
-              }}
-            />
+            <div id="timedate">
+              <input
+                id="inputtodo"
+                type="date"
+                name="date"
+                onChange={(e) => {
+                  this.handleChange(e);
+                }}
+              />
+              <input
+                id="inputtodo"
+                type="time"
+                name="time"
+                onChange={(e) => {
+                  this.handleChange(e);
+                }}
+              />
+            </div>
             <button
               onClick={(e) => {
                 this.handleAdd();
@@ -147,50 +163,52 @@ export class Todo extends React.Component {
             </button>
           </div>
         </div>
-        {todo?.map((item) => (
-          <div
-            id="tododiv"
-            style={{
-              color: item.status == true ? "white" : "black",
-              backgroundColor: item.status == true ? "green" : "",
-            }}
-            key={item.id}
-          >
+        <div id="show">
+          {todo?.map((item) => (
             <div
-              id="statusdiv"
+              id="tododiv"
               style={{
-                color: item.status == true ? "white" : "",
+                color: item.status == true ? "white" : "black",
+                backgroundColor: item.status == true ? "green" : "",
               }}
+              key={item.id}
             >
-              <input
-                id="checkbox"
-                type="checkbox"
-                checked={item.status}
-                onChange={this.handleChecked.bind(this, item.id, item.status)}
-              />
-              {/* <span>Mark&nbsp;</span> */}
-              <span>{item.status == true ? "Completed" : "In-complete"}</span>
-            </div>
-            <div id="titlediv">{item.title.task}</div>
-
-            <div id="date_time_div">
               <div
+                id="statusdiv"
                 style={{
-                  color: item.status == true ? "" : "blue",
+                  color: item.status == true ? "white" : "",
                 }}
               >
-                {new Date(item.title.time).toString().slice(0, 21)}
+                <input
+                  id="checkbox"
+                  type="checkbox"
+                  checked={item.status}
+                  onChange={this.handleChecked.bind(this, item.id, item.status)}
+                />
+                {/* <span>Mark&nbsp;</span> */}
+                <span>{item.status == true ? "Completed" : "In-complete"}</span>
               </div>
-              <hr />
-              <button
-                id="deletebtn"
-                onClick={this.handleDelete.bind(this, item.id)}
-              >
-                DELETE
-              </button>
+              <div id="titlediv">{item.title.task}</div>
+
+              <div id="date_time_div">
+                <div
+                  style={{
+                    color: item.status == true ? "" : "blue",
+                  }}
+                >
+                  {new Date(item.title.time).toString().slice(0, 21)}
+                </div>
+                <hr />
+                <button
+                  id="deletebtn"
+                  onClick={this.handleDelete.bind(this, item.id)}
+                >
+                  DELETE
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
         <div id="nextprevdiv">
           <button
