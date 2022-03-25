c0aaa4d8dc31ee16537228da49830a6bb1e8213b
Sandeep Yadav <yadavsandeep775@gmail.com>
2022-03-25 20:00:26 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./style.css";
-import { Counter } from "./Counter/Counter";
 import { Todo } from "./Todo/Todo";
 
 export default function App() {
   return (
     <div id="maindiv">
-      {/* <Counter title="Counter for my App" /> */}
       <Todo />
     </div>
   );
